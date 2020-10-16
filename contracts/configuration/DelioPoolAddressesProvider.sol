d83d531b2abf51efe187aeb9a58df84905a14888
DelioDucato <tripathi.kritesh@delio.in>
2020-10-16 09:44:50 +0900

    add configuration file

---
+++ contracts/configuration/DelioPoolAddressesProvider.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.0;
+
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+/**
+* @title DelioPoolAddressesProvider contract
+* @notice Is the main registry of the protocol. All the different components of the protocol are accessible
+* through the addresses provider.
+* @author Ducato
+**/
+
+contract DelioPoolAddressesProvider {
+}
